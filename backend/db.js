b9e2a652c78dac639c7d51be699ea5d9a2f1fd9d
ritesh <riteshsingh.py@gmail.com>
2022-06-23 22:33:09 +0530

    Progress and some refactors

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb://localhost:27017/cloudnote?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const User = require("../models/Users");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "Ritesh",
-    class: "10",
-  };
-  res.json(obj);
-});
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters long").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists." });
+      }
+
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Some error occured");
+    }
+  }
+);
 
 module.exports = router;
